7ccfbf9d4755f99757695e8f55e4e6ae3150d335
Marzan-Bryan <bryzan96@gmail.com>
2026-05-01 21:22:26 +0800

    Updated update.php

--- includes/update.php
+++ includes/update.php
@@ -3,15 +3,18 @@ include 'db.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $id = intval($_POST['id']);
-    $name = $_POST['name'];
-    $surname = $_POST['surname'];
-    $middlename = $_POST['middlename'];
-    $address = $_POST['address'];
-    $contact_number = $_POST['contact'];
+    $name = trim($_POST['name']);
+    $surname = trim($_POST['surname']);
+    $middlename = trim($_POST['middlename']);
+    $address = trim($_POST['address']);
+    $contact_number = trim($_POST['contact']);
+    $email = trim($_POST['email']);
+    $gender = trim($_POST['gender']);
+    $course = trim($_POST['course']);
+    $year_level = trim($_POST['year_level']);
 
     $students = getStudents();
     $updated = false;
-
 
     foreach ($students as &$student) {
         if ($student['id'] == $id) {
@@ -20,6 +23,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $student['middlename'] = $middlename;
             $student['address'] = $address;
             $student['contact_number'] = $contact_number;
+            $student['email'] = $email;
+            $student['gender'] = $gender;
+            $student['course'] = $course;
+            $student['year_level'] = $year_level;
             $updated = true;
             break;
         }
